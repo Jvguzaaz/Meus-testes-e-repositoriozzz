c0697d382500e316131ba8c1de866cf49df79189
Jvguzaaz <134119562+Jvguzaaz@users.noreply.github.com>
2025-09-24 22:22:43 -0300

    Add files via upload

---
+++ primeiropythonnavida.py
@@ -0,0 +1,38 @@
+while True:
+    value = int(input("Digite um valor: "))
+    if value < 20:
+        print("teu valor é baixo meno, tenta dnv")
+    elif value > 20:
+        print("teu valor ta muito alto meno, tenta dnv")
+    else:
+        print("NICEEEEEEEEEE. e nois mano GG, agr faz o quiz KKKKKKKKK")
+        break
+
+
+
+
+
+nome = input("Qual é o seu nome? ")
+print("tudo bao hj??,", nome)
+
+resposta = input("Responde aí, sim ou não? ")
+if resposta == "sim":
+    print("que bom meno")
+else:
+    resposta2 = input("pq nn mano??")
+    if resposta2 == "sla man":
+            resposta3 = print ("entao ta bom so escolhe entre 1 e 2 namoral")
+    opcao = input("1 ou 2? ")
+    if opcao == "1":
+              print("boa mano mas 1x1 = a 1 ne meu nobre")
+    elif opcao == "2":
+              print("boa escolha mano")
+    else:
+              print("tu nn sabe ler nao meno??")
+
+    chance = input("quer tentar de novo? ")
+
+    if chance == "sim":
+              print("entao reinicia o terminal caraio")
+    else:
+              print("ta bom meno flw")
